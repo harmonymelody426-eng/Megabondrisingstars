737f899aa11705140c13cf5a7eec0bb326d4725a
harmonymelody426-eng <harmonymelody426@gmail.com>
2026-07-10 22:18:57 +0700

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,8 +1,10 @@
-// Import library Supabase
+// Supabase Client Library
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-// Kredensial Asli Project Kamu
+// Kredensial Langsung Tanpa Menggunakan Environment Variable (Aman untuk GitHub Pages)
 const SUPABASE_URL = 'https://cprpizbcfvmwnumhkkwh.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNwcnBpemJjZnZtd251bWhra3doIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2Nzg3MjIsImV4cCI6MjA5OTI1NDcyMn0.teTEiRY0wA4FKrEN3xGYW8kZ79UTt3y4_m-XNpXGgWo';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+export { supabase };
